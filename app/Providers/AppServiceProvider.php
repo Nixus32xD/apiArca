1d9288699bf77a5a9a2c1c4ecc1d0de4d29a415b
Moron Gutierrez, Nicolás Uriel <94499134+Nixus32xD@users.noreply.github.com>
2026-08-31 07:54:41 -0300

    feat: bind sequence-aware WSFE client

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Services\Fiscal\Contracts\WsaaClient;
 use App\Services\Fiscal\Contracts\Wsfev1Client;
+use App\Services\Fiscal\SequenceAwareWsfev1Client;
 use App\Services\Fiscal\WSAAService;
 use App\Services\Fiscal\WSFEv1Service;
 use Illuminate\Support\ServiceProvider;
@@ -16,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(WsaaClient::class, WSAAService::class);
-        $this->app->bind(Wsfev1Client::class, WSFEv1Service::class);
+        $this->app->bind(Wsfev1Client::class, function ($app): Wsfev1Client {
+            return new SequenceAwareWsfev1Client($app->make(WSFEv1Service::class));
+        });
     }
 
     /**
